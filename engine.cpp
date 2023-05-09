e21e02a4c629792a93870982cd1ffe85351ed112
Jeremy Colegrove <jeremycolegrove2012@gmail.com>
2023-05-08 23:21:38 -0500

    working on make_move

--- engine.cpp
+++ engine.cpp
@@ -1,19 +1,90 @@
 #include "engine.h"
 
 
+// Move encoding (in bits):
+//  0- 5: origin square (6 bits)
+//  6-11: destination square (6 bits)
+// 12-14: moving piece (3 bits)
+// 15-17: captured piece (3 bits)
+// 18-20: special move flag (3 bits)
+constexpr uint64_t ENCODE_MOVE(int from, int to, int piece, int captured, MoveFlag flag) {
+    return (static_cast<uint64_t>(from) |
+            (static_cast<uint64_t>(to) << 6) |
+            (static_cast<uint64_t>(piece) << 12) |
+            (static_cast<uint64_t>(captured) << 15) |
+            (static_cast<uint64_t>(flag) << 18));
+}
+
+constexpr int MOVE_FROM(uint64_t move) { return move & 0x3F; }
+constexpr int MOVE_TO(uint64_t move) { return (move >> 6) & 0x3F; }
+constexpr int MOVE_PIECE(uint64_t move) { return (move >> 12) & 0x07; }
+constexpr int MOVE_CAPTURED(uint64_t move) { return (move >> 15) & 0x07; }
+constexpr MoveFlag MOVE_FLAG(uint64_t move) { return static_cast<MoveFlag>((move >> 18) & 0x07); }
+
+
+uint64_t *get_piece_bitboard(ChessBoard &board, int piece) {
+    switch (piece) {
+        case 0: return &board.white_pieces.pawns;
+        case 1: return &board.white_pieces.knights;
+        case 2: return &board.white_pieces.bishops;
+        case 3: return &board.white_pieces.rooks;
+        case 4: return &board.white_pieces.queens;
+        case 5: return &board.white_pieces.kings;
+        case 6: return &board.black_pieces.pawns;
+        case 7: return &board.black_pieces.knights;
+        case 8: return &board.black_pieces.bishops;
+        case 9: return &board.black_pieces.rooks;
+        case 10: return &board.black_pieces.queens;
+        case 11: return &board.black_pieces.kings;
+        default: return nullptr; // This should never happen if the piece value is valid
+    }
+}
+
 // Helper function to check if a bit is set at a given position
 bool is_bit_set(uint64_t bitboard, int position) {
     return (bitboard & (1ULL << position)) != 0;
 }
 
+void print_bitboard(uint64_t bitboard) {
+    for (int i=0; i<64; i++)
+    {
+        if ((i & 7) == 0) {
+            std::cout << std::endl;
+            std::cout << 8-(i/8) << "\t\t";
+        }
+        if (is_bit_set(bitboard, i)) std::cout << "1  ";
+        else std::cout << "0  ";
+    }
+    std::cout << "\n\n\t\ta  b  c  d  e  f  g  h" << std::endl;
+}
+
+
+void print_move(uint64_t move) {
+    int from_square = MOVE_FROM(move);
+    int to_square = MOVE_TO(move);
+    int piece = MOVE_PIECE(move);
+    int captured_piece = MOVE_CAPTURED(move);
+    MoveFlag flag = MOVE_FLAG(move);
+
+    std::cout << "Move: "
+              << static_cast<char>(from_square % 8 + 'a')
+              << static_cast<char>(8 - from_square / 8 + '0')
+              << static_cast<char>(to_square % 8 + 'a')
+              << static_cast<char>(8 - to_square / 8 + '0')
+              << " (piece: " << piece
+              << ", captured piece: " << captured_piece
+              << ", flag: " << static_cast<int>(flag) << ")"
+              << std::endl;
+}
+
 // Function to print the chess board
 void print_board(ChessBoard& board) {
     std::array<std::string, 12> pieceSymbols = {
+        "♟", "♞", "♝", "♜", "♛", "♚",  // Black pieces
         "♙", "♘", "♗", "♖", "♕", "♔", // White pieces
-        "♟", "♞", "♝", "♜", "♛", "♚"  // Black pieces
     };
 
-    std::array<U64 *, 12> pieceBitboards = {
+    std::array<U64*, 12> pieceBitboards = {
         &board.white_pieces.pawns,
         &board.white_pieces.knights,
         &board.white_pieces.bishops,
@@ -27,8 +98,9 @@ void print_board(ChessBoard& board) {
         &board.black_pieces.queens,
         &board.black_pieces.kings
     };
-for (int row = 7; row >= 0; --row) {
-        std::cout << (row + 1) << " ";
+
+    for (int row = 0; row < 8; row++) {
+        std::cout << 8 - row << " ";
         for (int col = 0; col < 8; ++col) {
             int position = row * 8 + col;
             bool pieceFound = false;
@@ -75,7 +147,52 @@ for (int row = 7; row >= 0; --row) {
 
     std::cout << "Half-move counter: " << board.half_move_counter << std::endl;
     std::cout << "Full-move counter: " << board.full_move_counter << std::endl;
+}
 
+
+void set_bit(U64 &bitboard, int position) {
+    bitboard |= (1ull << position);
+}
+
+void clear_bit(U64 &bitboard, int position) {
+    bitboard &= ~(1ull << position);
+}
+
+
+void make_move(ChessBoard &board, uint64_t move) {
+    int from_square = MOVE_FROM(move);
+    int to_square = MOVE_TO(move);
+    int piece = MOVE_PIECE(move);
+    int captured_piece = MOVE_CAPTURED(move);
+    MoveFlag flag = MOVE_FLAG(move);
+
+    // Clear the moving piece from the origin square
+    clear_bit(*get_piece_bitboard(board, piece), from_square);
+
+    // Set the moving piece in the destination square
+    set_bit(*get_piece_bitboard(board, piece), to_square);
+
+    // Remove the captured piece if any
+    if (captured_piece != -1) {
+        clear_bit(*get_piece_bitboard(board, captured_piece), to_square);
+    }
+
+    // Handle special move flags (e.g., promotion, en passant, castling)
+    if (flag != MoveFlag::NORMAL) {
+        // Implement the logic for handling special moves
+        switch (flag) {
+            case MoveFlag::PROMOTION_QUEEN:
+                std::cout << "ITS HERE" << std::endl;
+                clear_bit(*get_piece_bitboard(board, piece), to_square);
+                set_bit(*get_piece_bitboard(board, 5), to_square);
+        }
+    }
+
+    // Update en passant square, castling rights, side to move, and move counters
+    // ...
+
+    // Swap side to move
+    board.white_to_move = !board.white_to_move;
 }
 
 ChessBoard create_board_from_fen(const std::string& fen) {
@@ -146,8 +263,22 @@ ChessBoard create_board_from_fen(const std::string& fen) {
 }
 
 int main() {
-    std::string fen = "r1bqkbnr/pppp1ppp/2n5/4p3/4P3/5N2/PPPP1PPP/RNBQKB1R w KQkq - 0 3";
-    ChessBoard board = create_board_from_fen(fen);
+    ChessBoard board;
+
+    board = create_board_from_fen("8/4P3/8/8/8/8/8/8 w - - 0 1");
+
+    // Example move: e2e4 (White pawn moves from e2 to e4)
+    int from_square = e7; // e2
+    int to_square = e8; // e4
+    int piece = 0; // White pawn
+    int captured_piece = -1; // No piece captured
+    MoveFlag flag = MoveFlag::PROMOTION_QUEEN; // Normal move
+
+    uint64_t move = ENCODE_MOVE(from_square, to_square, piece, captured_piece, flag);
+    print_move(move);
+    make_move(board, move);
+
     print_board(board);
+
     return 0;
 }
--- engine.h
+++ engine.h
@@ -12,6 +12,18 @@
 
 using namespace std;
 
+enum {
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1, no_square
+};
+
+
 struct Bitboard {
     U64 pawns;
     U64 knights;
@@ -29,6 +41,17 @@ struct ChessBoard {
     int en_passant_square;
     unsigned half_move_counter;
     unsigned full_move_counter;
+};
+
+enum MoveFlag {
+    NORMAL = 0,
+    PROMOTION_QUEEN = 1,
+    PROMOTION_ROOK = 2,
+    PROMOTION_BISHOP = 3,
+    PROMOTION_KNIGHT = 4,
+    EN_PASSANT = 5,
+    KING_SIDE_CASTLING = 6,
+    QUEEN_SIDE_CASTLING = 7
 };
 
 #endif
